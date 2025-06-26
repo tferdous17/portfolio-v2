be1b402d9e605b75ef00132b7c226ce7c51e5514
tferdous17 <tferdous611@gmail.com>
2025-06-25 23:05:03 -0400

    navbar current prog

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import LandingPage from './components/page-ui/LandingPage'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
+      <Navbar />
       <LandingPage />
     </>
   )
