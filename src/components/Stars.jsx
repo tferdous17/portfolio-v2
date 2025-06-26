26ef91fb03f031a2791e9c55644e0466d345da78
tferdous17 <tferdous611@gmail.com>
2025-06-26 14:37:07 -0400

    create twinkling stars effect

---
+++ src/components/Stars.jsx
@@ -0,0 +1,15 @@
+import '../stars.css'
+
+export default function Stars() {
+  return (
+    <div className="stars-container">
+      <div className="star star1"></div>
+      <div className="star star2"></div>
+      <div className="star star3"></div>
+      <div className="star star4"></div>
+      <div className="star star5"></div>
+      <div className="star star6"></div>
+    </div>
+  )
+}
+
