b83fb773549ef30165211246eb801d8abb7843e0
tferdous17 <tferdous611@gmail.com>
2025-06-29 13:31:24 -0400

    add skills section to about

--- src/components/page-ui/About.jsx
+++ src/components/page-ui/About.jsx
@@ -18,23 +18,80 @@ export default function About() {
             engaging digital experiences. My interest in software development
             started close to 3 years ago and since then, I've been constantly
             learning, experimenting, and rebuilding software across various
-            domains—from distributed systems to fullstack applications. My primary
-            focus is distributed systems and backend development,
-            but I also enjoy creating websites that can leave a tangible impact.
+            domains—from distributed systems to fullstack applications. My
+            primary focus is distributed systems and backend development, but I
+            also enjoy creating websites that can leave a tangible impact.
           </p>
           <p className="text-gray-400 text-xl leading-relaxed mb-4">
-            On campus, I founded the Artificial Intelligence Club @ FSC where
-            me and my fellow officers have built an active community of
-            70+ members for those with a shared interest in A.I. I've also served
-            as an organizer, mentor and judge for <a href="https://www.ramhacks.com/" target="_blank" className="text-blue-400">RamHacks</a>, a new hackathon that
-            was able to get 80 participants and secure nearly $1,000 in prizes
-            its first time around. Not too bad!
+            On campus, I founded the Artificial Intelligence Club @ FSC where me
+            and my fellow officers have built an active community of 70+ members
+            for those with a shared interest in A.I. I've also served as an
+            organizer, mentor and judge for{" "}
+            <a
+              href="https://www.ramhacks.com/"
+              target="_blank"
+              className="text-blue-400"
+            >
+              RamHacks
+            </a>
+            , a new hackathon that was able to get 80 participants and secure
+            nearly $1,000 in prizes its first time around. Not too bad!
           </p>
-          <p className="text-gray-400 text-xl leading-relaxed mb-4">
-            Here's a few technologies I've been working with lately:
+          <p className="text-gray-400 text-xl leading-relaxed mb-18">
+            When I'm not coding, you can find me going on long-distance runs,
+            playing video games, traveling, or learning a new skill for fun!
           </p>
-          <div>
-            <ul className="grid grid-cols-2 gap-x-2 gap-y-1 pl-6 text-gray-400 font-mono">
+
+          <div className="">
+            <h2 className="font-inter font-bold text-3xl text-gray-300">
+              Skills
+            </h2>
+            <p className="text-gray-400 text-xl leading-relaxed mt-3 mb-4">
+              And here's some technologies I'm familiar with:
+            </p>
+            <div className="flex flex-col gap-3">
+              <h3 className="text-lg font-bold font-inter text-gray-300 mb-1">
+                Languages
+              </h3>
+              <div className="flex flex-wrap gap-2 mb-2">
+                {languages.map((lang) => (
+                  <span
+                    key={lang}
+                    className="inline-flex items-center bg-[#A29BFE]/10 text-purple-300 font-mono text-xs sm:text-sm px-3 py-1 rounded-full border border-purple-500/20 shadow-sm hover:bg-purple-400/20 transition-colors duration-200 cursor-pointer"
+                  >
+                    {lang}
+                  </span>
+                ))}
+              </div>
+              <h3 className="text-lg font-bold font-inter text-gray-300 mb-1">
+                Frameworks
+              </h3>
+              <div className="flex flex-wrap gap-2 mb-2">
+                {frameworks.map((fr) => (
+                  <span
+                    key={fr}
+                    className="inline-flex items-center bg-[#A29BFE]/10 text-purple-300 font-mono text-xs sm:text-sm px-3 py-1 rounded-full border border-purple-500/20 shadow-sm hover:bg-purple-400/20 transition-colors duration-200 cursor-pointer"
+                  >
+                    {fr}
+                  </span>
+                ))}
+              </div>
+              <h3 className="text-lg font-bold font-inter text-gray-300 mb-1">
+                Tools/Other
+              </h3>
+              <div className="flex flex-wrap gap-2 mb-100">
+                {tools.map((tool) => (
+                  <span
+                    key={tool}
+                    className="inline-flex items-center bg-[#A29BFE]/10 text-purple-300 font-mono text-xs sm:text-sm px-3 py-1 rounded-full border border-purple-500/20 shadow-sm hover:bg-purple-400/20 transition-colors duration-200 cursor-pointer"
+                  >
+                    {tool}
+                  </span>
+                ))}
+              </div>
+            </div>
+
+            {/* <ul className="grid grid-cols-2 gap-x-2 gap-y-1 pl-6 text-gray-400 font-mono">
                 <li className="relative pl-5 before:content-['➤'] before:absolute before:left-0 before:text-[#A29BFE]">Java</li>
                 <li className="-mx-35 relative pl-5 before:content-['➤'] before:absolute before:left-0 before:text-[#A29BFE]">Golang</li>
                 <li className="relative pl-5 before:content-['➤'] before:absolute before:left-0 before:text-[#A29BFE]">Python</li>
@@ -43,10 +100,49 @@ export default function About() {
                 <li className="-mx-35 relative pl-5 before:content-['➤'] before:absolute before:left-0 before:text-[#A29BFE]">Vue</li>
                 <li className="relative pl-5 before:content-['➤'] before:absolute before:left-0 before:text-[#A29BFE]">Node.js</li>
                 <li className="-mx-35 relative pl-5 before:content-['➤'] before:absolute before:left-0 before:text-[#A29BFE]">Express</li>
-            </ul>
+            </ul> */}
           </div>
         </div>
       </div>
     </div>
   );
 }
+
+const languages = [
+  "Java",
+  "Golang",
+  "Python",
+  "JavaScript (ES6+)",
+  "TypeScript",
+  "React",
+  "Vue",
+  "Node.js",
+  "Express",
+  "HTML/CSS",
+  "Protobuf",
+];
+const frameworks = [
+  "Vue",
+  "React",
+  "Express",
+  "Next.js",
+  "FastAPI",
+  "Vitest",
+  "Jest",
+  "TensorFlow",
+  "Spring Boot",
+  "JUnit",
+];
+const tools = [
+  "Git",
+  "Docker",
+  "Kubernetes",
+  "AWS",
+  "Google Cloud",
+  "Node",
+  "gRPC",
+  "PostgreSQL",
+  "SQLite",
+  "Supabase",
+  "OpenCV",
+];
